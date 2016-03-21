c7d36089ba44b776f078774e063fe2c95f9a0f08
Travis Smith <travissmith94@hotmail.com>
2016-03-21 14:22:08 -0500

    Set up Flask structure

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+import sys 
+import logging
+
+app = Flask(__name__)
+
+#See Error Logs
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+app.config['SECRET_KEY'] = 'mnIcdwVxqc80HQ0nYOtS'
+
+
