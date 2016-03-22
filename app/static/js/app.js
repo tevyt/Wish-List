6bcdb6bc5e7a5bca058fe6c9615bfd6e4b6b58fe
Travis Smith <travissmith94@hotmail.com>
2016-03-22 12:06:17 -0500

    Removed templates folder

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,6 +3,7 @@
 var wishListApp = angular.module('wishListApp', ['ngRoute']);
 wishListApp.controller('SignUpController' , ['$scope' , '$http',
         function($scope , $http){
+            $scope.profile_url = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ1uNq8D2knHJ6qlIybMOrI-Q1c0GXAXpsQH5L6qtwOeyuG6b-bGOLDk1M"
             $scope.user = {};
         }]);
 
