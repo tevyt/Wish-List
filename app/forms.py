e51782dee07bc2e7814b8e5081cc4730edb2024b
Travis Smith <travissmith94@hotmail.com>
2016-03-23 12:06:45 -0500

    Basic validations

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,10 @@
 from flask.ext.wtf import Form
 from wtforms.fields import TextField
-from wtforms.validators import Required
+from wtforms.validators import Required , Email, EqualTo
 
 class SignUpForm(Form):
     firstname = TextField('firstname' , validators=[Required()])
     lastname = TextField('lastname' , validators=[Required()])
-    email  = TextField('email' , validators=[Required()])
+    email  = TextField('email' , validators=[Required() , Email()])
+    password = TextField('password' , validators=[Required() , EqualTo('confirmation')])
+    confirmation = TextField('confirmation' , validators=[Required()])
