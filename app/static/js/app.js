1153d21b5e413e08e6855a2c58e26a66cc8e50ea
Travis Smith <travissmith94@hotmail.com>
2016-03-23 20:56:06 -0500

    Set up cookies for login

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,14 +1,19 @@
 'use strict';
 
-var wishListApp = angular.module('wishListApp', ['ngRoute']);
-wishListApp.controller('SignUpController' , ['$scope' , '$http', '$log',
-        function($scope , $http, $log){
+var wishListApp = angular.module('wishListApp', ['ngRoute' , 'ngCookies']).
+run(function($cookies){
+    if(!$cookies.get('loggedIn')){
+        $cookies.put('loggedIn' , false);
+    }
+});
+
+wishListApp.controller('SignUpController' , ['$scope' , '$http', '$log', '$location',
+        function($scope , $http, $log, $location){
             $scope.user = {};
             $scope.validate = function(){
                 $http.post('/signup' , $scope.user).
                     success(function(data , status){
-                        $log.log('Status: ' + status);
-                        $log.log('Data: ' + JSON.stringify(data));
+                        $location.path('/login');
                     }).
                 error(function(data , status){
                     $log.log('Status: ' + status);
@@ -17,11 +22,23 @@ wishListApp.controller('SignUpController' , ['$scope' , '$http', '$log',
             };
         }]);
 
+wishListApp.controller('LoginController' , ['$scope', '$cookies',  function($scope , $cookies){
+    $scope.login = function(){
+        $cookies.put('loggedIn' , true);
+    };
+}]);
+
+
 wishListApp.config(function($routeProvider){
     $routeProvider.when('/signup',{
         templateUrl: 'static/js/partials/sign_up.html',
         controller: 'SignUpController',
-    }).otherwise({
+    }).
+    when('/login' , {
+        templateUrl: 'static/js/partials/login.html',
+        controller: 'LoginController'
+    }).
+    otherwise({
         redirectTo: '/signup'
     })
 });
