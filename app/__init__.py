b8b65c35f13ffce70b6e64c81684bbd31a66c233
Travis Smith <travissmith94@hotmail.com>
2016-03-28 20:54:03 -0500

    Set up Procfile for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #See Error Logs
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://travis:wishlist@localhost/wishlist"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://dlwxmgeohpnlcx:rUM6HI77Lhp6gYSqygwb_cGqj4@ec2-54-235-199-36.compute-1.amazonaws.com:5432/d89u8tpsgnf8c4"
 db = SQLAlchemy(app)
 from app import views
 from app.models import User
